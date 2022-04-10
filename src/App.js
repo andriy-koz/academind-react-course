22c155a0b996d8eebac3d8700b9d10bf2ff62264
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-04-10 04:33:10 -0300

    used-useEffect-&-useCallback-hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,20 +8,19 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  // Added useCallback to generate a memoized callback, wich only
+  // changes if one of specified dependencies change.
+  // This is used to prevent unnecesary renders.
+  // And to pass that callback to the useEffect depencies hook,
+  // as a good practice.
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    // Use try/catch to catch any error
     try {
       const response = await fetch('https://swapi.dev/api/films/');
-
-      // Check for response .ok, also could check for status
-      // Throw an error with random message
-      // We don´t need to handle this with AXIOS
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
-
       const data = await response.json();
       const transformedMovies = data.results.map(movieData => {
         return {
@@ -34,14 +32,20 @@ function App() {
       });
       setMovies(transformedMovies);
     } catch (error) {
-      // Catch thrown error
-      setError(error.message); // Set error state
+      setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
 
-  // Use this method to render things conditionally
-  // without using a lot of && operators
+  // If we use a pointer to the pure function in the dependencies array of useEffect,
+  // it will create an infinite loop. Because the function is an object and it will change
+  // every time the component renders.
+  // We can leave the dependencies array empty, to execute the useEffect function only the first
+  // time the component renders. But that is not a good practice.
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
   let content = <p>Found no movies.</p>;
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
