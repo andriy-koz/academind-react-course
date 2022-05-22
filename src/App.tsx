ff8a32be15965cc10ed75d3b5c79578092c2ba69
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-05-22 12:44:26 -0300

    working with Props and adding a Data Model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
-  return <div></div>;
+  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
