f008489fb588b7f37eb9963299187c20d8854989
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-05-22 22:07:47 -0300

    using context

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
+import { TodosContext } from '../store/todos-context';
 import classes from './NewTodo.module.css';
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = props => {
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -11,7 +13,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = props => {
       // throw an error
       return;
     }
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
 
   return (
